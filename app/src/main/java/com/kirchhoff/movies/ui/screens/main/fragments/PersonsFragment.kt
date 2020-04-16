fff74d7e6b4f04866e5a59f40d20364c11ae4f33
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-04-16 19:34:57 +0300

    Issue-6: Added stub for MainActivity.

---
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/main/fragments/PersonsFragment.kt
@@ -0,0 +1,6 @@
+package com.kirchhoff.movies.ui.screens.main.fragments
+
+import androidx.fragment.app.Fragment
+import com.kirchhoff.movies.R
+
+class PersonsFragment : Fragment(R.layout.fragment_persons)

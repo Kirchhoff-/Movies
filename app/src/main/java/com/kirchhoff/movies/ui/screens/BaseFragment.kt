a1f8377044e1496ed1e554b3eb0b1dc869ab2e22
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-07-07 21:59:12 +0300

    Issue-35: Added BaseFragment.kt.

---
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/BaseFragment.kt
@@ -0,0 +1,12 @@
+package com.kirchhoff.movies.ui.screens
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+abstract class BaseFragment(@LayoutRes layoutId: Int) : Fragment(layoutId) {
+    protected fun <T> LiveData<T>.subscribe(func: (T) -> Unit) {
+        this.observe(viewLifecycleOwner, Observer { func(it) })
+    }
+}

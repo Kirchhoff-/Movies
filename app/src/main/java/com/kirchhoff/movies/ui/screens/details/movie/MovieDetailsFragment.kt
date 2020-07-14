ed8f6d2da2a1bf423f01eb6bf9f8ae94440acad9
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-07-14 21:00:04 +0300

    Issue-44: Added stub for MoviesDetailsFragment. Create DetailsActivity and implements transition to it after click on item in MoviesFragment.kt

---
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/details/movie/MovieDetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.kirchhoff.movies.ui.screens.details.movie
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.kirchhoff.movies.R
+import com.kirchhoff.movies.data.Movie
+import com.kirchhoff.movies.ui.screens.BaseFragment
+
+class MovieDetailsFragment : BaseFragment(R.layout.fragment_movie_details) {
+
+    private val movie: Movie by lazy { arguments!!.getParcelable<Movie>(MOVIE_ARG)!! }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvTitle: TextView = view.findViewById(R.id.tvTitle)
+
+        tvTitle.text = movie.title
+    }
+
+    companion object {
+        fun newInstance(movie: Movie): MovieDetailsFragment {
+            val fragment = MovieDetailsFragment()
+            val arg = Bundle()
+            arg.putParcelable(MOVIE_ARG, movie)
+            fragment.arguments = arg
+            return fragment
+        }
+
+        private const val MOVIE_ARG = "MOVIE_ARG"
+    }
+}

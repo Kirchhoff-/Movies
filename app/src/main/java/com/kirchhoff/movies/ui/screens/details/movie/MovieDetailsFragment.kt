f2e92ed761c790ab883b6570fd5a4ee1d3d565e8
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-07-15 18:30:40 +0300

    Issue-44: Small fixed in layout_movie_details_content.xml. Added base implementation for MovieDetailsFragment.kt.

--- app/src/main/java/com/kirchhoff/movies/ui/screens/details/movie/MovieDetailsFragment.kt
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/details/movie/MovieDetailsFragment.kt
@@ -2,9 +2,14 @@ package com.kirchhoff.movies.ui.screens.details.movie
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.kirchhoff.movies.R
 import com.kirchhoff.movies.data.Movie
+import com.kirchhoff.movies.data.responses.MovieDetails
+import com.kirchhoff.movies.databinding.FragmentMovieDetailsBinding
+import com.kirchhoff.movies.extensions.downloadPoster
 import com.kirchhoff.movies.ui.screens.BaseFragment
+import com.kirchhoff.movies.utils.binding.viewBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MovieDetailsFragment : BaseFragment(R.layout.fragment_movie_details) {
@@ -12,6 +17,7 @@ class MovieDetailsFragment : BaseFragment(R.layout.fragment_movie_details) {
     private val movie: Movie by lazy { arguments!!.getParcelable<Movie>(MOVIE_ARG)!! }
 
     private val vm by viewModel<MovieDetailsVM>()
+    private val viewBinding: FragmentMovieDetailsBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,59 @@ class MovieDetailsFragment : BaseFragment(R.layout.fragment_movie_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(viewBinding) {
+            content.tvMovieTitle.text = movie.title
+            content.ivMoviePoster.downloadPoster(movie.poster_path)
+            ivBackdrop.downloadPoster(movie.backdrop_path)
+        }
+
+        with(vm) {
+            movieDetails.subscribe(::handleMovieDetailsData)
+            loading.subscribe(::handleLoading)
+            error.subscribe { handleError() }
+            exception.subscribe { handleException() }
+        }
+    }
+
+    private fun handleMovieDetailsData(movieDetails: MovieDetails) {
+        with (viewBinding.content) {
+            groupData.isVisible = true
+
+            tvReleaseDate.isVisible = movieDetails.release_date != null
+            tvReleaseDate.text = movieDetails.release_date
+
+            if (movieDetails.runtime != null) {
+                tvRuntime.isVisible = true
+                tvRuntime.text = movieDetails.runtime.toString()
+            } else {
+                tvRuntime.isVisible = false
+            }
+
+            if (movieDetails.production_countries.isNotEmpty()) {
+                tvCountry.isVisible = true
+                tvCountry.text = movieDetails.production_countries.first().name
+            } else {
+                tvCountry.isVisible = false
+            }
+
+            tvTagline.isVisible = movieDetails.tagline != null
+            tvTagline.text = movieDetails.tagline
+
+            tvOverview.text = movieDetails.overview
+        }
+    }
+
+    private fun handleLoading(visible: Boolean) {
+        viewBinding.content.groupLoading.isVisible = visible
+    }
+
+    private fun handleError() {
+        viewBinding.content.groupError.isVisible = true
+    }
+
+    private fun handleException() {
+        viewBinding.content.groupException.isVisible = true
     }
 
     companion object {

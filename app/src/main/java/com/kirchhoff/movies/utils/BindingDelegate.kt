9f72e30a63935d32a99235c9704994e68077577e
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-08-27 19:31:56 +0300

    Issue-96: Refactored logic of binding view.

---
+++ app/src/main/java/com/kirchhoff/movies/utils/BindingDelegate.kt
@@ -0,0 +1,62 @@
+package com.kirchhoff.movies.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.MainThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.observe
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+@Suppress("unused")
+class FragmentViewBindingDelegate<T : ViewBinding>(
+    val fragment: Fragment,
+    val viewBindingFactory: (View) -> T
+) : ReadOnlyProperty<Fragment, T> {
+    private var binding: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : LifecycleObserver {
+
+            @MainThread
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                    viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+
+                        @MainThread
+                        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                        fun onDestroy() {
+                            binding = null
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        val binding = binding
+        if (binding != null) {
+            return binding
+        }
+
+        val lifecycle = fragment.viewLifecycleOwner.lifecycle
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
+        }
+
+        return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
+    }
+}
+
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(crossinline bindingInflater: (LayoutInflater) -> T) =
+    lazy(LazyThreadSafetyMode.NONE) { bindingInflater.invoke(layoutInflater) }
--- app/src/main/java/com/kirchhoff/movies/utils/binding/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.kirchhoff.movies.utils.binding
-
-import android.os.Looper
-
-internal fun checkIsMainThread() = check(isMainThread)
-
-internal inline val isMainThread: Boolean
-    get() = Looper.myLooper() == Looper.getMainLooper()

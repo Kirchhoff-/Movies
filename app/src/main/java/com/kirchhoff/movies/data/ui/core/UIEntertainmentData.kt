fb2e2519805476ced4ad3cd4633ba81ff615f3db
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-12-21 18:10:13 +0200

    Issue-158: Wrote tests for CoreMapper. Refactored UIEntertainmentPerson sealed class. Added missed annotation for name filed (NetworkObjectWithName class)

--- app/src/main/java/com/kirchhoff/movies/data/ui/core/UIEntertainmentData.kt
+++ app/src/main/java/com/kirchhoff/movies/data/ui/core/UIEntertainmentData.kt
@@ -7,16 +7,28 @@ data class UIEntertainmentCredits(
     val crew: List<UIEntertainmentPerson.Creator>?
 )
 
-sealed class UIEntertainmentPerson(val id: Int, val name: String, val profilePath: String?) {
-    class Actor(id: Int, name: String, profilePath: String?, val character: String?) :
-        UIEntertainmentPerson(id, name, profilePath), CreditsView.CreditsInfo {
+sealed class UIEntertainmentPerson(
+    open val id: Int,
+    open val name: String,
+    open val profilePath: String?
+) {
+    data class Actor(
+        override val id: Int,
+        override val name: String,
+        override val profilePath: String?,
+        private val character: String?
+    ) : UIEntertainmentPerson(id, name, profilePath), CreditsView.CreditsInfo {
         override fun title() = name
         override fun description() = character
         override fun imagePath() = profilePath
     }
 
-    class Creator(id: Int, name: String, profilePath: String?, val job: String?) :
-        UIEntertainmentPerson(id, name, profilePath), CreditsView.CreditsInfo {
+    data class Creator(
+        override val id: Int,
+        override val name: String,
+        override val profilePath: String?,
+        private val job: String?
+    ) : UIEntertainmentPerson(id, name, profilePath), CreditsView.CreditsInfo {
         override fun title() = name
         override fun description() = job
         override fun imagePath() = profilePath

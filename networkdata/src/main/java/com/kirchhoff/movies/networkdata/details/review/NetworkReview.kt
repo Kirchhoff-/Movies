1b1aa958976f536330ddf08d30af0e8ef7ca92f3
Kirchhoff- <kirchhoff.19.92@gmail.com>
2023-02-06 14:21:27 +0200

    Issue-274: Added user avatar to review's card.

--- networkdata/src/main/java/com/kirchhoff/movies/networkdata/details/review/NetworkReview.kt
+++ networkdata/src/main/java/com/kirchhoff/movies/networkdata/details/review/NetworkReview.kt
@@ -1,6 +1,9 @@
 package com.kirchhoff.movies.networkdata.details.review
 
+import com.google.gson.annotations.SerializedName
+
 data class NetworkReview(
     val author: String,
-    val content: String
+    val content: String,
+    @SerializedName("author_details") val authorDetails: NetworkReviewAuthorDetails
 )

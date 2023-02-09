2818b550e4d2566321cd3705863608fddeac659d
Dmitriy Gorbunov <kirchhoff.19.92@gmail.com>
2023-02-09 14:29:57 +0200

    Issue-275: Added rating to review's card

--- networkdata/src/main/java/com/kirchhoff/movies/networkdata/details/review/NetworkReviewAuthorDetails.kt
+++ networkdata/src/main/java/com/kirchhoff/movies/networkdata/details/review/NetworkReviewAuthorDetails.kt
@@ -2,4 +2,7 @@ package com.kirchhoff.movies.networkdata.details.review
 
 import com.google.gson.annotations.SerializedName
 
-data class NetworkReviewAuthorDetails(@SerializedName("avatar_path") val avatar: String?)
+data class NetworkReviewAuthorDetails(
+    @SerializedName("avatar_path") val avatar: String?,
+    val rating: Int?
+)

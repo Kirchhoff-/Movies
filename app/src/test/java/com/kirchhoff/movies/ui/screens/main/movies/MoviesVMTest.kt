4fd2ec676713c7c2625cddfc8ae85d951d7714e1
Kirchhoff- <kirchhoff.19.92@gmail.com>
2023-02-22 18:21:52 +0200

    Issue-291: Fixed tests

--- app/src/test/java/com/kirchhoff/movies/ui/screens/main/movies/MoviesVMTest.kt
+++ app/src/test/java/com/kirchhoff/movies/ui/screens/main/movies/MoviesVMTest.kt
@@ -1,7 +1,7 @@
 package com.kirchhoff.movies.ui.screens.main.movies
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.kirchhoff.movies.data.ui.core.UIPaginated
+import com.kirchhoff.movies.core.ui.paginated.UIPaginated
 import com.kirchhoff.movies.data.ui.main.UIMovie
 import com.kirchhoff.movies.repository.Result
 import com.kirchhoff.movies.repository.discover.IDiscoverRepository

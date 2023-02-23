ad4cef7c773f59f07b077136a6b4aafd26a45d76
Kirchhoff- <kirchhoff.19.92@gmail.com>
2023-02-23 18:11:33 +0200

    Issue-313: Added viewBinding to ReviewDetailsFragment

--- app/src/main/java/com/kirchhoff/movies/ui/screens/reviews/details/ReviewDetailsFragment.kt
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/reviews/details/ReviewDetailsFragment.kt
@@ -3,13 +3,15 @@ package com.kirchhoff.movies.ui.screens.reviews.details
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import com.kirchhoff.movies.R
 import com.kirchhoff.movies.core.ui.BaseFragment
+import com.kirchhoff.movies.core.ui.utils.viewBinding
 import com.kirchhoff.movies.data.ui.details.review.UIReview
+import com.kirchhoff.movies.databinding.FragmentReviewDetailsBinding
 
 class ReviewDetailsFragment : BaseFragment(R.layout.fragment_review_details) {
+
+    private val viewBinding by viewBinding(FragmentReviewDetailsBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,13 +19,12 @@ class ReviewDetailsFragment : BaseFragment(R.layout.fragment_review_details) {
         val review = requireArguments().getParcelable<UIReview>(REVIEW_ARG)!!
         val title = requireArguments().getString(TITLE_ARG)
 
-        val tvReviewContent: TextView = view.findViewById(R.id.tvReviewContent)
-        val toolbarReviewDetails: Toolbar = view.findViewById(R.id.toolbarReviewDetails)
-
-        tvReviewContent.movementMethod = ScrollingMovementMethod()
-        tvReviewContent.text = review.content
-        toolbarReviewDetails.title = getString(R.string.review_details_title_format, review.author, title)
-        toolbarReviewDetails.setNavigationOnClickListener { requireActivity().onBackPressed() }
+        with(viewBinding) {
+            tvReviewContent.movementMethod = ScrollingMovementMethod()
+            tvReviewContent.text = review.content
+            toolbarReviewDetails.title = getString(R.string.review_details_title_format, review.author, title)
+            toolbarReviewDetails.setNavigationOnClickListener { requireActivity().onBackPressed() }
+        }
     }
 
     companion object {

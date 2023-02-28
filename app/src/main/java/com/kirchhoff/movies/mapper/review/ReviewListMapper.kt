a805edd7c5e9f082398b38efbb6b66be7edda85e
Dmitriy Gorbunov <kirchhoff.19.92@gmail.com>
2023-02-28 17:28:49 +0200

    Issue-347: Moved BaseMapper to core module

--- app/src/main/java/com/kirchhoff/movies/mapper/review/ReviewListMapper.kt
+++ app/src/main/java/com/kirchhoff/movies/mapper/review/ReviewListMapper.kt
@@ -1,9 +1,9 @@
 package com.kirchhoff.movies.mapper.review
 
+import com.kirchhoff.movies.core.mapper.BaseMapper
 import com.kirchhoff.movies.core.repository.Result
 import com.kirchhoff.movies.core.ui.paginated.UIPaginated
 import com.kirchhoff.movies.data.ui.details.review.UIReview
-import com.kirchhoff.movies.mapper.BaseMapper
 import com.kirchhoff.movies.networkdata.core.NetworkPaginated
 import com.kirchhoff.movies.networkdata.details.review.NetworkReview
 

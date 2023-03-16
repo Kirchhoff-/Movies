c94cbabecec0193fc72959bbededc87c80c5b8c6
Kirchhoff- <kirchhoff.19.92@gmail.com>
2023-03-16 17:25:35 +0200

    Issue-375: Created separate view for displaying rating RatingTextView.

--- app/src/main/java/com/kirchhoff/movies/ui/screens/core/tvs/adapter/TvVH.kt
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/core/tvs/adapter/TvVH.kt
@@ -3,24 +3,22 @@ package com.kirchhoff.movies.ui.screens.core.tvs.adapter
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.isVisible
 import com.kirchhoff.movies.R
 import com.kirchhoff.movies.core.extensions.downloadPoster
 import com.kirchhoff.movies.core.extensions.inflate
 import com.kirchhoff.movies.core.ui.recyclerview.adapter.viewholder.BaseVH
 import com.kirchhoff.movies.data.ui.main.UITv
-import java.text.DecimalFormat
+import com.kirchhoff.movies.keywordsview.RatingTextView
 
 class TvVH(parent: ViewGroup) : BaseVH<UITv>(parent.inflate(R.layout.item_tv)) {
 
     private val ivTvPoster: ImageView = itemView.findViewById(R.id.ivTvPoster)
     private val tvTvTitle: TextView = itemView.findViewById(R.id.tvTvTitle)
-    private val tvTvRating: TextView = itemView.findViewById(R.id.tvTvRating)
+    private val tvTvRating: RatingTextView = itemView.findViewById(R.id.tvTvRating)
 
     override fun bind(item: UITv) {
         tvTvTitle.text = item.name
         ivTvPoster.downloadPoster(item.posterPath)
-        tvTvRating.isVisible = item.voteAverage != null
-        tvTvRating.text = DecimalFormat("#.0").format(item.voteAverage)
+        tvTvRating.displayRating(item.voteAverage)
     }
 }

d388a51d6d9633823cf83b1274b1bd7ae19f3ccd
Dmitriy Gorbunov <kirchhoff.19.92@gmail.com>
2023-05-13 10:45:28 +0300

    Issue-437: Router in review module created

---
+++ screen/review/src/main/java/com/kirchhoff/movies/screen/review/router/ReviewRouter.kt
@@ -0,0 +1,16 @@
+package com.kirchhoff.movies.screen.review.router
+
+import androidx.appcompat.app.AppCompatActivity
+import com.kirchhoff.movies.screen.review.R
+import com.kirchhoff.movies.screen.review.data.UIReview
+import com.kirchhoff.movies.screen.review.ui.screen.details.ReviewDetailsFragment
+
+internal class ReviewRouter(private val activity: AppCompatActivity) : IReviewRouter {
+    override fun openDetailsScreen(review: UIReview, title: String) {
+        activity.supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, ReviewDetailsFragment.newInstance(review, title))
+            .addToBackStack(null)
+            .commit()
+    }
+}

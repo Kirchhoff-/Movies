3e923a15227540e9d9128bb6d5237dadf511ae8f
Kirchhoff- <kirchhoff.19.92@gmail.com>
2023-08-22 10:34:16 +0300

    Issue-508: Moved a few methods from IRouter to IMovieRouter

--- screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/router/MovieRouter.kt
+++ screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/router/MovieRouter.kt
@@ -2,13 +2,37 @@ package com.kirchhoff.movies.screen.movie.router
 
 import androidx.appcompat.app.AppCompatActivity
 import com.kirchhoff.movies.core.data.UIGenre
+import com.kirchhoff.movies.core.data.UIMovie
 import com.kirchhoff.movies.screen.movie.R
+import com.kirchhoff.movies.screen.movie.ui.screen.country.MovieCountryFragment
 import com.kirchhoff.movies.screen.movie.ui.screen.genre.MovieListByGenreFragment
+import com.kirchhoff.movies.screen.movie.ui.screen.similar.MovieSimilarFragment
 
 class MovieRouter(private val activity: AppCompatActivity) : IMovieRouter {
+
     override fun openMoviesByGenreScreen(genre: UIGenre) {
         activity.supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, MovieListByGenreFragment.newInstance(genre))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun openMoviesByCountryScreen(countryId: String, countryName: String) {
+        activity.supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragmentContainer,
+                MovieCountryFragment.newInstance(countryId, countryName)
+            )
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun openSimilarMoviesScreen(movie: UIMovie) {
+        activity.supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragmentContainer,
+                MovieSimilarFragment.newInstance(movie.id, movie.title)
+            )
             .addToBackStack(null)
             .commit()
     }

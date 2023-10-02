058658ec927d1e06d603ad2a55834f809a31416d
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-10-02 15:05:11 +0300

    Issue-547: Changed placeholder for movies without image

--- view/creditsview/src/main/java/com/kirchhoff/movies/creditsview/data/CreditsInfo.kt
+++ view/creditsview/src/main/java/com/kirchhoff/movies/creditsview/data/CreditsInfo.kt
@@ -1,24 +1,28 @@
 package com.kirchhoff.movies.creditsview.data
 
+import androidx.annotation.DrawableRes
 import com.kirchhoff.movies.core.data.UIEntertainmentPerson
 
 data class CreditsInfo(
     val id: Int,
     val title: String?,
     val description: String?,
-    val imagePath: String?
+    val imagePath: String?,
+    @DrawableRes val placeholderImageResources: Int
 ) {
     constructor(actor: UIEntertainmentPerson.Actor) : this(
         actor.id,
         actor.name,
         actor.character,
-        actor.profilePath
+        actor.profilePath,
+        com.kirchhoff.movies.core.R.drawable.ic_empty_avatar
     )
 
     constructor(creator: UIEntertainmentPerson.Creator) : this(
         creator.id,
         creator.name,
         creator.job,
-        creator.profilePath
+        creator.profilePath,
+        com.kirchhoff.movies.core.R.drawable.ic_empty_avatar
     )
 }

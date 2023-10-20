0d6a1d15b9a035dac2810bf8494d1dd2f20d717e
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-10-20 17:30:12 +0300

    Issue-579: IDashboardRouter with it implementation added

--- app/src/main/java/com/kirchhoff/movies/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/kirchhoff/movies/ui/screens/main/MainActivity.kt
@@ -2,10 +2,10 @@ package com.kirchhoff.movies.ui.screens.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kirchhoff.movies.core.router.IRouter
 import com.kirchhoff.movies.core.ui.utils.viewBinding
 import com.kirchhoff.movies.databinding.ActivityWithFragmentBinding
 import com.kirchhoff.movies.router.routerModule
+import com.kirchhoff.movies.ui.screens.main.router.IDashboardRouter
 import org.koin.android.ext.android.inject
 import org.koin.core.context.loadKoinModules
 import org.koin.core.parameter.parametersOf
@@ -14,12 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     private val viewBinding by viewBinding(ActivityWithFragmentBinding::inflate)
 
-    private val router: IRouter by inject { parametersOf(this) }
+    private val dashboardRouter: IDashboardRouter by inject { parametersOf(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         loadKoinModules(routerModule)
+        loadKoinModules(dashboardModule)
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-        router.openDiscoverScreen()
+        dashboardRouter.openDashboard()
     }
 }

e0b944fb5a8c6a456ae49249a3880447c97bb25c
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-11-18 14:00:40 +0200

    CreditsCastUI added

--- screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/cast/ui/CreditsCastItemUI.kt
+++ screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/cast/ui/CreditsCastItemUI.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.kirchhoff.movies.core.data.UIEntertainmentPerson
 import com.kirchhoff.movies.core.extensions.BASE_POSTER_PATH
@@ -21,25 +27,38 @@ import kotlin.random.Random
 
 @Composable
 internal fun CreditsCastItemUI(actor: UIEntertainmentPerson.Actor) {
-    Row(
-        modifier = Modifier
-            .height(200.dp)
-            .fillMaxWidth()
+    Card(
+        modifier = Modifier.shadow(10.dp),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        AsyncImage(
+        Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .width(60.dp),
-            model = BASE_POSTER_PATH + actor.profilePath,
-            contentScale = ContentScale.Crop,
-            placeholder = painterResource(com.kirchhoff.movies.core.R.drawable.ic_empty_avatar),
-            error = painterResource(com.kirchhoff.movies.core.R.drawable.ic_empty_avatar),
-            contentDescription = ""
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = actor.name)
-            Text(text = actor.character.orEmpty())
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(140.dp),
+                model = BASE_POSTER_PATH + actor.profilePath,
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(com.kirchhoff.movies.core.R.drawable.ic_empty_avatar),
+                error = painterResource(com.kirchhoff.movies.core.R.drawable.ic_empty_avatar),
+                contentDescription = ""
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                Text(
+                    text = actor.name,
+                    fontSize = 16.sp,
+                    color = colorResource(com.kirchhoff.movies.core.R.color.text_main)
+                )
+                Text(
+                    text = actor.character.orEmpty(),
+                    fontSize = 14.sp,
+                    color = colorResource(com.kirchhoff.movies.core.R.color.text_hint)
+                )
+            }
         }
     }
 }
--- screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/cast/ui/CreditsCastUI.kt
+++ screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/cast/ui/CreditsCastUI.kt
@@ -1,12 +1,15 @@
 package com.kirchhoff.movies.screen.credits.ui.screen.cast.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kirchhoff.movies.core.data.UIEntertainmentPerson
 import com.kirchhoff.movies.core.ui.compose.MoviesToolbar
 import com.kirchhoff.movies.core.utils.StringValue
@@ -24,15 +27,17 @@ internal fun CreditsCastUI(
         MoviesToolbar(title = screenState.title.asString(context)) {
             onBackPressed.invoke()
         }
-    }
-
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(
-            count = screenState.actors.size,
-            itemContent = {
-                CreditsCastItemUI(screenState.actors[it])
-            }
-        )
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+        ) {
+            items(
+                count = screenState.actors.size,
+                itemContent = {
+                    CreditsCastItemUI(screenState.actors[it])
+                }
+            )
+        }
     }
 }
 

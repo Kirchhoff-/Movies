6f87a71e252d590622f1cdf4c7c57efa5cd666c5
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-11-21 12:27:23 +0200

    Improved CreditsCrewPersonItemUI

--- screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/persons/CreditsCrewPersonItemUI.kt
+++ screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/persons/CreditsCrewPersonItemUI.kt
@@ -1,7 +1,5 @@
 package com.kirchhoff.movies.screen.credits.ui.screen.crew.ui.items.persons
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,9 +8,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -48,6 +46,7 @@ internal fun CreditsCrewPersonItemUI(
                 error = painterResource(com.kirchhoff.movies.core.R.drawable.ic_empty_avatar),
                 contentDescription = ""
             )
+            Divider(color = Color.Gray)
             Text(
                 modifier = Modifier
                     .fillMaxSize()

42df88b73c74a55cae0f00a58fb6d90315178523
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-11-21 12:18:58 +0200

    Improve ua

--- screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/persons/CreditsCrewPersonItemUI.kt
+++ screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/persons/CreditsCrewPersonItemUI.kt
@@ -1,25 +1,29 @@
 package com.kirchhoff.movies.screen.credits.ui.screen.crew.ui.items.persons
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.kirchhoff.movies.core.extensions.BASE_POSTER_PATH
-import com.kirchhoff.movies.screen.credits.R
 import com.kirchhoff.movies.screen.credits.ui.screen.crew.model.CreditsCrewListPersonItem
 
 @Composable
@@ -30,7 +34,7 @@ internal fun CreditsCrewPersonItemUI(
         modifier = Modifier
             .height(250.dp)
             .width(150.dp),
-        elevation = 2.dp,
+        elevation = 4.dp,
         shape = RoundedCornerShape(8.dp)
     ) {
         Column {
@@ -45,9 +49,13 @@ internal fun CreditsCrewPersonItemUI(
                 contentDescription = ""
             )
             Text(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight(),
                 text = person.name,
                 fontSize = 16.sp,
-                color = colorResource(com.kirchhoff.movies.core.R.color.text_main),
+                textAlign = TextAlign.Center,
+                color = colorResource(com.kirchhoff.movies.core.R.color.text_main)
             )
         }
     }

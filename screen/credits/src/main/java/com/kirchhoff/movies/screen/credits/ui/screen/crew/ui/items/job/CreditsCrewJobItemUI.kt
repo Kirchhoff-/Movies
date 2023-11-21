eb25fbf6755b21813e25ee7de84923ba28049c4c
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-11-21 12:25:40 +0200

    Click listener improved

--- screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/job/CreditsCrewJobItemUI.kt
+++ screen/credits/src/main/java/com/kirchhoff/movies/screen/credits/ui/screen/crew/ui/items/job/CreditsCrewJobItemUI.kt
@@ -3,6 +3,7 @@ package com.kirchhoff.movies.screen.credits.ui.screen.crew.ui.items.job
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,8 +33,12 @@ internal fun CreditsCrewJobItemUI(
         modifier = Modifier.fillMaxWidth()
             .height(48.dp)
             .background(Color.LightGray)
-            .padding(horizontal = 16.dp)
-            .clickable { onItemClick.invoke(jobText) },
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(bounded = true),
+                onClick = { onItemClick.invoke(jobText) }
+            )
+            .padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(

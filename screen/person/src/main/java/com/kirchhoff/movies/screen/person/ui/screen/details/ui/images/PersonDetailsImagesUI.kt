e0cb0f7b4902acc7111a86e3abe89e4c54c3355d
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2023-11-29 16:41:10 +0200

    Added indicator

--- screen/person/src/main/java/com/kirchhoff/movies/screen/person/ui/screen/details/ui/images/PersonDetailsImagesUI.kt
+++ screen/person/src/main/java/com/kirchhoff/movies/screen/person/ui/screen/details/ui/images/PersonDetailsImagesUI.kt
@@ -1,14 +1,26 @@
 package com.kirchhoff.movies.screen.person.ui.screen.details.ui.images
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -23,19 +35,41 @@ internal fun PersonDetailsImagesUI(images: List<UIPersonImage>) {
         pageCount = { images.size }
     )
 
-    HorizontalPager(
-        modifier = Modifier
-            .height(300.dp)
-            .fillMaxWidth(),
-        beyondBoundsPageCount = 2,
-        state = pagerState
-    ) { page ->
-        AsyncImage(
-            modifier = Modifier.fillMaxSize(),
-            model = BASE_POSTER_PATH + images[page].url,
-            contentScale = ContentScale.Crop,
-            contentDescription = ""
-        )
+    Box {
+        HorizontalPager(
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            beyondBoundsPageCount = 2,
+            state = pagerState
+        ) { page ->
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = BASE_POSTER_PATH + images[page].url,
+                contentScale = ContentScale.Crop,
+                contentDescription = ""
+            )
+        }
+
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) colorResource(com.kirchhoff.movies.core.R.color.colorPrimary) else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(8.dp)
+                )
+            }
+        }
     }
 }
 

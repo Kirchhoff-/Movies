5c213bf92ec6f594e73ded1d7905be7a2d866c16
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-01 17:16:19 +0200

    Stub for MovieDetailsScreenState and MovieDetailsViewModel

---
+++ screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/model/MovieDetailsScreenState.kt
@@ -0,0 +1,21 @@
+package com.kirchhoff.movies.screen.movie.ui.screen.details.model
+
+import com.kirchhoff.movies.core.data.UIEntertainmentCredits
+import com.kirchhoff.movies.core.data.UIImage
+import com.kirchhoff.movies.core.data.UIMovie
+import com.kirchhoff.movies.core.utils.StringValue
+import com.kirchhoff.movies.screen.movie.data.UIMovieDetails
+import com.kirchhoff.movies.screen.movie.data.UITrailersList
+
+internal data class MovieDetailsScreenState(
+    val title: StringValue,
+    val backdropPath: String?,
+    val posterPath: String?,
+    val details: UIMovieDetails,
+    val trailers: UITrailersList,
+    val credits: UIEntertainmentCredits,
+    val similarMovies: List<UIMovie>,
+    val images: List<UIImage>,
+    val isLoading: Boolean,
+    val errorMessage: StringValue
+)

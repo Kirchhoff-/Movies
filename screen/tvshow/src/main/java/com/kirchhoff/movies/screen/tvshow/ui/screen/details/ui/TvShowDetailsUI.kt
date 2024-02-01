c8be4f8b9aa82aa1b96c6ef228d274d8e211a1a2
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-01 11:20:16 +0200

    TvShowDetailsUI improvements

--- screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/ui/screen/details/ui/TvShowDetailsUI.kt
+++ screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/ui/screen/details/ui/TvShowDetailsUI.kt
@@ -1,20 +1,29 @@
 package com.kirchhoff.movies.screen.tvshow.ui.screen.details.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -52,7 +61,7 @@ internal fun TvShowDetailsUI(
 
         when {
             screenState.isLoading -> ShowLoading()
-            screenState.errorMessage.asString(LocalContext.current).isNotEmpty() -> ShowError()
+            screenState.errorMessage.asString(LocalContext.current).isNotEmpty() -> ShowError(screenState)
             else -> ShowUI(
                 screenState = screenState,
                 onCreditItemClick = onCreditItemClick,
@@ -64,12 +73,27 @@ internal fun TvShowDetailsUI(
 
 @Composable
 private fun ShowLoading() {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
 }
 
 @Composable
-private fun ShowError() {
+private fun ShowError(screenState: TvShowDetailsScreenState) {
+    val context = LocalContext.current
+    val error = screenState.errorMessage.asString(context)
+    var errorMessage by rememberSaveable { mutableStateOf("") }
 
+    if (error.isNotEmpty() && errorMessage.isEmpty()) {
+        errorMessage = error
+        SideEffect {
+            Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+        }
+    }
 }
 
 @ExperimentalLayoutApi

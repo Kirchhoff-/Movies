90e0896549002978ad05dbf75c16d0d40e5cc4ac
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-03 10:33:38 +0200

    Working on MovieDetailsUI

--- screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/ui/MovieDetailsUI.kt
+++ screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/ui/MovieDetailsUI.kt
@@ -1,12 +1,39 @@
 package com.kirchhoff.movies.screen.movie.ui.screen.details.ui
 
-import androidx.compose.foundation.layout.Box
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.kirchhoff.movies.core.R
+import com.kirchhoff.movies.core.data.UIEntertainmentCredits
+import com.kirchhoff.movies.core.extensions.BASE_POSTER_PATH
 import com.kirchhoff.movies.core.ui.compose.MoviesToolbar
+import com.kirchhoff.movies.core.utils.StringValue
+import com.kirchhoff.movies.screen.movie.data.UIMovieDetails
+import com.kirchhoff.movies.screen.movie.data.UITrailersList
 import com.kirchhoff.movies.screen.movie.ui.screen.details.model.MovieDetailsScreenState
 
 @Composable
@@ -20,6 +47,97 @@ internal fun MovieDetailsUI(
             onBackPressed = { onBackPressed.invoke() }
         )
 
-        Box(modifier = Modifier.fillMaxSize())
+        when {
+            screenState.isLoading -> ShowLoading()
+            screenState.errorMessage.asString(LocalContext.current).isNotEmpty() -> ShowError(screenState)
+            else -> ShowUI(
+                screenState = screenState
+            )
+        }
     }
+}
+
+@Composable
+private fun ShowLoading() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ShowError(screenState: MovieDetailsScreenState) {
+    val context = LocalContext.current
+    val error = screenState.errorMessage.asString(context)
+    var errorMessage by rememberSaveable { mutableStateOf("") }
+
+    if (error.isNotEmpty() && errorMessage.isEmpty()) {
+        errorMessage = error
+        SideEffect {
+            Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
+@Composable
+private fun ShowUI(
+    screenState: MovieDetailsScreenState
+) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        AsyncImage(
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            model = BASE_POSTER_PATH + screenState.backdropPath,
+            contentScale = ContentScale.Crop,
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(start = 16.dp),
+            text = screenState.title.asString(LocalContext.current),
+            color = colorResource(R.color.text_main),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Preview
+@Composable
+private fun MovieDetailsUIPreview() {
+    MovieDetailsUI(
+        screenState = MovieDetailsScreenState(
+            title = StringValue.SimpleText(""),
+            backdropPath = "",
+            posterPath = "",
+            details = UIMovieDetails(
+                productionCountries = emptyList(),
+                runtime = 0,
+                tagLine = "",
+                overview = "",
+                releaseDate = "",
+                voteCount = 0,
+                voteAverage = 0f,
+                genres = emptyList()
+            ),
+            trailers = UITrailersList(
+                results = emptyList()
+            ),
+            credits = UIEntertainmentCredits(
+                cast = emptyList(),
+                crew = emptyList()
+            ),
+            similarMovies = emptyList(),
+            images = emptyList(),
+            isLoading = false,
+            errorMessage = StringValue.Empty
+        ),
+        onBackPressed = {}
+    )
 }

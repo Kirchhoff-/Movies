008ab472286877fe840b70dc221d191e3d5b2405
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-05 12:54:32 +0200

    Improved MovieDetailsSimilarItemUI

--- screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/ui/similar/MovieDetailsSimilarItemUI.kt
+++ screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/ui/similar/MovieDetailsSimilarItemUI.kt
@@ -5,36 +5,32 @@ package com.kirchhoff.movies.screen.movie.ui.screen.details.ui.similar
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.kirchhoff.movies.core.R
 import com.kirchhoff.movies.core.data.UIMovie
 import com.kirchhoff.movies.core.extensions.BASE_POSTER_PATH
-import com.kirchhoff.movies.core.R
+import com.kirchhoff.movies.keywordsview.RatingText
 
 @Composable
 internal fun MovieDetailsSimilarItemUI(
@@ -53,23 +49,31 @@ internal fun MovieDetailsSimilarItemUI(
         elevation = 4.dp,
         shape = RoundedCornerShape(8.dp)
     ) {
-        Column {
-            AsyncImage(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(190.dp),
-                model = BASE_POSTER_PATH + movie.posterPath,
-                contentScale = ContentScale.Crop,
-                placeholder = painterResource(R.drawable.ic_empty_movie),
-                error = painterResource(R.drawable.ic_empty_movie),
-                contentDescription = ""
-            )
-            Divider(color = Color.Gray)
-            Spacer(modifier = Modifier.height(4.dp))
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box {
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(190.dp),
+                    model = BASE_POSTER_PATH + movie.posterPath,
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(R.drawable.ic_empty_movie),
+                    error = painterResource(R.drawable.ic_empty_movie),
+                    contentDescription = ""
+                )
+                RatingText(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd),
+                    voteAverage = movie.voteAverage
+                )
+            }
             Text(
-                modifier = Modifier.padding(horizontal = 4.dp),
                 text = movie.title,
                 fontSize = 16.sp,
+                textAlign = TextAlign.Center,
                 color = colorResource(R.color.text_main),
                 overflow = TextOverflow.Ellipsis
             )

06d295d6bd225b72d41186ccd927458681796812
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-05 15:03:19 +0200

    Old files removed

--- screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/facade/MovieFacade.kt
+++ screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/facade/MovieFacade.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import com.kirchhoff.movies.core.data.UIMovie
 import com.kirchhoff.movies.screen.movie.IMovieFacade
 import com.kirchhoff.movies.screen.movie.ui.screen.details.MovieDetailsFragment
-import com.kirchhoff.movies.screen.movie.ui.screen.details.OldMovieDetailsFragment
 import com.kirchhoff.movies.screen.movie.ui.screen.list.MovieListFragment
 
 class MovieFacade : IMovieFacade {
--- screen/movie/src/main/java/com/kirchhoff/movies/screen/movie/ui/screen/details/OldMovieDetailsModule.kt
+++
@@ -1,8 +0,0 @@
-package com.kirchhoff.movies.screen.movie.ui.screen.details
-
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-internal val oldMovieDetailsModule = module {
-    viewModel { OldMovieDetailsViewModel(movieRepository = get()) }
-}

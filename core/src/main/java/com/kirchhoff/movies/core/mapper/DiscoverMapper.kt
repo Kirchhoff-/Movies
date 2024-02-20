89f665c6ca65f680b55a79df5d69b0f6f8301f8f
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-20 11:01:11 +0200

    Issue-441: Removed movie mapping form IDiscoverMapper

--- core/src/main/java/com/kirchhoff/movies/core/mapper/DiscoverMapper.kt
+++ core/src/main/java/com/kirchhoff/movies/core/mapper/DiscoverMapper.kt
@@ -1,20 +1,12 @@
 package com.kirchhoff.movies.core.mapper
 
-import com.kirchhoff.movies.core.data.UIMovie
 import com.kirchhoff.movies.core.data.UITv
 import com.kirchhoff.movies.core.repository.Result
 import com.kirchhoff.movies.core.ui.paginated.UIPaginated
 import com.kirchhoff.movies.networkdata.core.NetworkPaginated
-import com.kirchhoff.movies.networkdata.main.NetworkMovie
 import com.kirchhoff.movies.networkdata.main.NetworkTv
 
 class DiscoverMapper : BaseMapper(), IDiscoverMapper {
-
-    override fun createUIDiscoverMovieList(discoverMovies: Result<NetworkPaginated<NetworkMovie>>): Result<UIPaginated<UIMovie>> =
-        when (discoverMovies) {
-            is Result.Success -> Result.Success(createUIDiscoverMovies(discoverMovies.data))
-            else -> mapErrorOrException(discoverMovies)
-        }
 
     override fun createUIDiscoverTvList(discoverTvs: Result<NetworkPaginated<NetworkTv>>): Result<UIPaginated<UITv>> =
         when (discoverTvs) {
@@ -22,27 +14,11 @@ class DiscoverMapper : BaseMapper(), IDiscoverMapper {
             else -> mapErrorOrException(discoverTvs)
         }
 
-    private fun createUIDiscoverMovies(response: NetworkPaginated<NetworkMovie>) =
-        UIPaginated(
-            response.page,
-            response.results.map { createUIMovie(it) },
-            response.totalPages
-        )
-
     private fun createUIDiscoverTvs(response: NetworkPaginated<NetworkTv>) =
         UIPaginated(
             response.page,
             response.results.map { createUITv(it) },
             response.totalPages
-        )
-
-    private fun createUIMovie(movie: NetworkMovie) =
-        UIMovie(
-            movie.id,
-            movie.title,
-            movie.posterPath,
-            movie.backdropPath,
-            movie.voteAverage
         )
 
     private fun createUITv(tv: NetworkTv) =

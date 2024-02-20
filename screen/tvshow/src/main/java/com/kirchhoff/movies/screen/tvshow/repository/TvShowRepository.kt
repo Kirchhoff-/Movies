74e511e41d01c951f99b26c4a6f02d4f5f22e0a1
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-02-20 11:10:57 +0200

    Issue-441: ITvShowListMapper and it's implementation created

--- screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/repository/TvShowRepository.kt
+++ screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/repository/TvShowRepository.kt
@@ -2,19 +2,19 @@ package com.kirchhoff.movies.screen.tvshow.repository
 
 import com.kirchhoff.movies.core.data.UIEntertainmentCredits
 import com.kirchhoff.movies.core.data.UITv
-import com.kirchhoff.movies.core.mapper.IDiscoverMapper
 import com.kirchhoff.movies.core.repository.BaseRepository
 import com.kirchhoff.movies.core.repository.Result
 import com.kirchhoff.movies.core.ui.paginated.UIPaginated
 import com.kirchhoff.movies.screen.tvshow.data.UITvShowInfo
 import com.kirchhoff.movies.screen.tvshow.mapper.details.ITvShowDetailsMapper
+import com.kirchhoff.movies.screen.tvshow.mapper.list.ITvShowListMapper
 import com.kirchhoff.movies.screen.tvshow.network.TvShowService
 import com.kirchhoff.movies.storage.tvshow.IStorageTvShow
 
 internal class TvShowRepository(
     private val tvService: TvShowService,
     private val tvDetailsMapper: ITvShowDetailsMapper,
-    private val discoverMapper: IDiscoverMapper,
+    private val tvShowListMapper: ITvShowListMapper,
     private val tvShowStorage: IStorageTvShow
 ) : BaseRepository(), ITvShowRepository {
 
@@ -25,11 +25,11 @@ internal class TvShowRepository(
             result.data.results.forEach { tvShowStorage.updateInfo(it) }
         }
 
-        return discoverMapper.createUIDiscoverTvList(result)
+        return tvShowListMapper.createTvShowList(result)
     }
 
     override suspend fun fetchSimilarTvShows(tvId: Int, page: Int): Result<UIPaginated<UITv>> =
-        discoverMapper.createUIDiscoverTvList(
+        tvShowListMapper.createTvShowList(
             apiCall {
                 tvService.fetchSimilarTvShows(tvId, page)
             }

1bcc547cace597039b8c76b8e9d938329e42ab08
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-06-27 15:30:37 +0300

    Issue-688: Removed code duplication from router

--- screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/router/TvShowRouter.kt
+++ screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/router/TvShowRouter.kt
@@ -1,9 +1,8 @@
 package com.kirchhoff.movies.screen.tvshow.router
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.kirchhoff.movies.core.R
 import com.kirchhoff.movies.core.data.TvId
+import com.kirchhoff.movies.core.extensions.replaceFragment
 import com.kirchhoff.movies.screen.tvshow.ui.screen.list.TvShowListFragment
 
 internal interface ITvShowRouter {
@@ -17,29 +16,22 @@ internal interface ITvShowRouter {
 internal class TvShowRouter(private val activity: AppCompatActivity) : ITvShowRouter {
 
     override fun openSimilarTvShowScreen(tvId: TvId) {
-        replaceFragment(TvShowListFragment.similar(tvId))
+        activity.replaceFragment(TvShowListFragment.similar(tvId))
     }
 
     override fun openAiringTodayScreen() {
-        replaceFragment(TvShowListFragment.airingToday())
+        activity.replaceFragment(TvShowListFragment.airingToday())
     }
 
     override fun openOnTheAirScreen() {
-        replaceFragment(TvShowListFragment.onTheAir())
+        activity.replaceFragment(TvShowListFragment.onTheAir())
     }
 
     override fun openPopularScreen() {
-        replaceFragment(TvShowListFragment.popular())
+        activity.replaceFragment(TvShowListFragment.popular())
     }
 
     override fun openTopRatedScreen() {
-        replaceFragment(TvShowListFragment.topRated())
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        activity.supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .addToBackStack(null)
-            .commit()
+        activity.replaceFragment(TvShowListFragment.topRated())
     }
 }

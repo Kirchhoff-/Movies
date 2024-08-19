69a8a7a6e497e31dd4ed5e7c8d498e78f3839f2c
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2024-08-19 17:13:58 +0300

    Issue-740: Moved error mapping out of TvShowDetailsMapper

--- screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/ui/screen/details/usecase/TvShowDetailsUseCase.kt
+++ screen/tvshow/src/main/java/com/kirchhoff/movies/screen/tvshow/ui/screen/details/usecase/TvShowDetailsUseCase.kt
@@ -3,6 +3,7 @@ package com.kirchhoff.movies.screen.tvshow.ui.screen.details.usecase
 import com.kirchhoff.movies.core.data.TvId
 import com.kirchhoff.movies.core.data.UIEntertainmentCredits
 import com.kirchhoff.movies.core.data.UITv
+import com.kirchhoff.movies.core.mapper.ICoreMapper
 import com.kirchhoff.movies.core.repository.Result
 import com.kirchhoff.movies.core.ui.paginated.UIPaginated
 import com.kirchhoff.movies.screen.tvshow.ui.screen.details.mapper.ITvShowDetailsMapper
@@ -17,15 +18,25 @@ internal interface ITvShowDetailsUseCase {
 
 internal class TvShowDetailsUseCase(
     private val tvShowDetailsRepository: ITvShowDetailsRepository,
-    private val tvShowDetailsMapper: ITvShowDetailsMapper
+    private val tvShowDetailsMapper: ITvShowDetailsMapper,
+    private val coreMapper: ICoreMapper
 ) : ITvShowDetailsUseCase {
 
     override suspend fun fetchDetails(id: TvId): Result<TvShowDetailsInfo> =
-        tvShowDetailsMapper.createUITvDetails(tvShowDetailsRepository.fetchDetails(id))
+        when (val response = tvShowDetailsRepository.fetchDetails(id)) {
+            is Result.Success -> Result.Success(tvShowDetailsMapper.createUITvDetails(response.data))
+            else -> response.mapErrorOrException()
+        }
 
     override suspend fun fetchCredits(id: TvId): Result<UIEntertainmentCredits> =
-        tvShowDetailsMapper.createUIEntertainmentCredits(tvShowDetailsRepository.fetchCredits(id))
+        when (val response = tvShowDetailsRepository.fetchCredits(id)) {
+            is Result.Success -> Result.Success(coreMapper.createUIEntertainmentCredits(response.data))
+            else -> response.mapErrorOrException()
+        }
 
     override suspend fun fetchSimilar(id: TvId, page: Int): Result<UIPaginated<UITv>> =
-        tvShowDetailsMapper.createTvShowList(tvShowDetailsRepository.fetchSimilar(id, page))
+        when (val response = tvShowDetailsRepository.fetchSimilar(id, page)) {
+            is Result.Success -> Result.Success(tvShowDetailsMapper.createTvShowList(response.data))
+            else -> response.mapErrorOrException()
+        }
 }

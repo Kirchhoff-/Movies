1d15b654b9be38e1412e2b31f8c9fc7860409d2f
Dmitriy Gorbunov <dmitriy.gorbunov.work@gmail.com>
2026-04-07 11:27:11 +0300

    Issue-762: Enable HTTP logging for debug builds

--- app/src/main/java/com/kirchhoff/movies/di/NetworkModule.kt
+++ app/src/main/java/com/kirchhoff/movies/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.kirchhoff.movies.di
 import com.kirchhoff.movies.BuildConfig
 import com.kirchhoff.movies.network.interceptors.ApiKeyInterceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,6 +12,7 @@ val networkModule = module {
     single {
         OkHttpClient.Builder()
             .addInterceptor(ApiKeyInterceptor())
+            .addInterceptor(HttpLoggingInterceptor().setLevel(if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE))
             .build()
     }
 
